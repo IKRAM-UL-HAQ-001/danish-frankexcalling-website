33768d8e97a73adf0f5d00001441843537fb2274
IKRAM-UL-HAQ-001 <mrikramulhaq0005@gmail.com>
2026-05-11 23:58:15 +0500

    seeder issues resolve

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,26 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->updateOrInsert(
+            ['id' => 1],
+            [
+                'name' => 'vQapttK1zNq3BELO6MiasA==',
+                'email' => 'NsRoqbf7mFkWqpt3PNALmsrJM7Uxa1cbOEu17Ro7caI=',
+                'password' => Hash::make('acR9EaNKAAh8PdK0uSAgaA=='),
+                'role' => 'admin',
+                'status' => 'active',
+                'exchange_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
     }
 }
